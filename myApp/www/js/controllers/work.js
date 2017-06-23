b7e31d27d3b51a1429283f21a56f6958ab9fc9d6
chu <chuer89@gmail.com>
2017-06-23 19:00:18 +0800

    up

--- myApp/www/js/controllers/work.js
+++ myApp/www/js/controllers/work.js
@@ -8,7 +8,7 @@ angular.module('work.controller', [])
     $scope.itemsCrm = workPlatform.crm();
     $scope.itemsLearn = workPlatform.learn();
 
-    $http.post('http://123.206.95.25:8080/kuaidao/client/resources.html', {
+    $http.post('http://123.206.95.25:18080/kuaidao/client/resources.html', {
     	"appType":"IOS","appVersion":"1.0.0","body":{},"businessType":"departmrnt_info"
     }).success(function(data) {
 
